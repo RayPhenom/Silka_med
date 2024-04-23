35ec2d03de48b3162755b0e5747332c2abb8ae88
RayPhenom <lumuonix@gmail.com>
2024-04-24 01:33:45 +0300

    update in top Navbar

--- silka-med/src/components/Navbar.jsx
+++ silka-med/src/components/Navbar.jsx
@@ -3,8 +3,6 @@ import { Link } from "react-scroll";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
-    
- 
 
     return (
         <div className="fixed w-full z-10 text-white">
@@ -67,7 +65,63 @@ const Navbar = () => {
                             Contact Us
                         </button>
                     </div>
-
+                   ///
+                </div>
+                <div
+                    className={
+                        '${menu ? "translate-x-0" : "-translate-x-full"} lg:hidden flex  flex-col absolute bg-backgroundColor text-white left-0 top-16 font-semibold text-2xl  tect-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-300'
+                    }
+                >
+                    <Link
+                        to="home"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        className="hover:text-hoverColor transition-all cursor-pointer"
+                    >
+                        Home
+                    </Link>
+                    <Link
+                        to="about"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        className="hover:text-hoverColor transition-all cursor-pointer"
+                    >
+                        About Us
+                    </Link>
+                    <Link
+                        to="services"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        className="hover:text-hoverColor transition-all cursor-pointer"
+                    >
+                        Services
+                    </Link>
+                    <Link
+                        to="doctors"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        className="hover:text-hoverColor transition-all cursor-pointer"
+                    >
+                        Doctors
+                    </Link>
+                    <Link
+                        to="blog"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        className="hover:text-hoverColor transition-all cursor-pointer"
+                    >
+                        Blog
+                    </Link>
+                    <div className="lg:hidden">
+                        <button className="bg-brightColor text-white px-4 py-2 rounded-md hover:bg-hoverColor transition duration-300 ease-in-out">
+                            Contact Us
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
