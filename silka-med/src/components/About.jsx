fa0afac569cd98c1d05a54572dbac4657a84247c
RayPhenom <lumuonix@gmail.com>
2024-05-02 15:49:50 +0300

    About page done

--- silka-med/src/components/About.jsx
+++ silka-med/src/components/About.jsx
@@ -3,30 +3,39 @@ import img from "../assets/img/about.jpg"
 
 const About = () => {
   return (
-    <div className="">
-      <div>
-        <h1>About Us</h1>
-        <p>
+    <div className="min-h-screen flex flex-col lg:flex-row justify-between items-center lg:px-32 px-5 pt-24 lg:pt-16 gap-5">
+      <div className="w-full lg:w-3/4 space-y-4">
+        <h1 className="text-4xl font-semibold text-center lg:text-start">About Us</h1>
+        <p className="text-justify lg:text-start">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, eum
+          magni reprehenderit laudantium reiciendis ducimus aperiam debitis
+          fugit unde possimus labore eveniet minus nihil explicabo voluptatum
+          rerum exercitationem sequi. Rerum!Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, eum
           magni reprehenderit laudantium reiciendis ducimus aperiam debitis
           fugit unde possimus labore eveniet minus nihil explicabo voluptatum
           rerum exercitationem sequi. Rerum!
         </p>
-        <p>
+        <p className="text-justify lg:text-start">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, eum
+          magni reprehenderit laudantium reiciendis ducimus aperiam debitis
+          fugit unde possimus labore eveniet minus nihil explicabo voluptatum
+          rerum exercitationem sequi. Rerum!Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, eum
           magni reprehenderit laudantium reiciendis ducimus aperiam debitis
           fugit unde possimus labore eveniet minus nihil explicabo voluptatum
           rerum exercitationem sequi. Rerum!
         </p>
-        <p>
+        <p className="text-justify lg:text-start">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, eum
+          magni reprehenderit laudantium reiciendis ducimus aperiam debitis
+          fugit unde possimus labore eveniet minus nihil explicabo voluptatum
+          rerum exercitationem sequi. Rerum!Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, eum
           magni reprehenderit laudantium reiciendis ducimus aperiam debitis
           fugit unde possimus labore eveniet minus nihil explicabo voluptatum
           rerum exercitationem sequi. Rerum!
         </p>
       </div>
-      <div>
-        <img src={img} alt="" />
+      <div className="w-full lg:w-1/2">
+        <img src={img} alt="img" className="rounded-lg" /> 
       </div>
     </div>
   );
